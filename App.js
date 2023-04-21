eced3913b367059d13e1d6e1c48d45475576494d
KESHAVsCODE <106483384+KESHAVsCODE@users.noreply.github.com>
2023-04-22 00:24:03 +0530

    create design and add functionality

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
 const detailContainer = document.getElementById("center-details");
+const input = document.querySelector("input");
+const cityBtn = document.getElementById("city");
+const stateBtn = document.getElementById("state");
+const centerBtn = document.getElementById("center");
+const searchIcon = document.querySelector("i");
+
+let activeBtn = null;
 
 let centersData = {};
 async function getISROCentersData() {
@@ -9,25 +16,93 @@ async function getISROCentersData() {
       throw new Error("Not Found");
     }
     centersData = await response.json();
-    printISROCentersData(centersData);
+    printISROCentersData();
   } catch (error) {
     console.log(error.message);
   }
 }
 
-async function printISROCentersData(data) {
+async function printISROCentersData() {
+  detailContainer.innerHTML = "";
   const fragment = document.createDocumentFragment();
-  console.log(data.centres);
-  for (const center of data.centres) {
+
+  for (const center of centersData.centres) {
+    const parent = document.createElement("div");
+    parent.classList.add("centers-info");
     for (const key in center) {
       if (key !== "id") {
         const child = document.createElement("p");
         child.textContent = center[key];
-        fragment.appendChild(child);
+        parent.appendChild(child);
       }
     }
+    fragment.appendChild(parent);
   }
   detailContainer.append(fragment);
 }
 
 getISROCentersData();
+
+function searchCenters() {
+  const inputKey = input.value.toLowerCase();
+  //console.log(inputKey);
+
+  detailContainer.innerHTML = "";
+
+  if (!activeBtn) {
+    detailContainer.textContent = "!. . . . Please Select A Category . . .!";
+    return;
+  }
+
+  if (!inputKey) {
+    detailContainer.textContent = `!. . . . Please Enter Valid ${activeBtn.id} . . .!`;
+    return;
+  }
+
+  let finderKey = activeBtn.name;
+  const fragment = document.createDocumentFragment();
+
+  //console.log(finderKey);
+
+  for (const center of centersData.centres) {
+    if (center[finderKey].toLowerCase().includes(inputKey)) {
+      const parent = document.createElement("div");
+      parent.classList.add("centers-info");
+      for (const key in center) {
+        if (key !== "id") {
+          const child = document.createElement("p");
+          child.textContent = center[key];
+          parent.appendChild(child);
+        }
+      }
+      fragment.appendChild(parent);
+    }
+  }
+
+  if (!fragment.childElementCount) {
+    detailContainer.textContent = `!. . . . Please Enter Valid ${activeBtn.id.toUpperCase()} . . .!`;
+    return;
+  }
+
+  detailContainer.append(fragment);
+}
+
+function activate(event) {
+  event.target.classList.toggle("active");
+  if (activeBtn && event.target.id === activeBtn.id) {
+    activeBtn = null;
+    printISROCentersData();
+    //console.log(activeBtn);
+    return;
+  }
+  if (activeBtn) activeBtn.classList.toggle("active");
+
+  activeBtn = event.target;
+  //console.log(activeBtn);
+}
+
+cityBtn.addEventListener("click", activate);
+stateBtn.addEventListener("click", activate);
+centerBtn.addEventListener("click", activate);
+
+searchIcon.addEventListener("click", searchCenters);
