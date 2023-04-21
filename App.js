94139632ee5576a6418e4f662b80472c98ac21c6
KESHAVsCODE <106483384+KESHAVsCODE@users.noreply.github.com>
2023-04-21 17:11:19 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,1 +1,33 @@
+const detailContainer = document.getElementById("center-details");
 
+let centersData = {};
+async function getISROCentersData() {
+  const apiUrl = "https://isro.vercel.app/api/centres";
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error("Not Found");
+    }
+    centersData = await response.json();
+    printISROCentersData(centersData);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function printISROCentersData(data) {
+  const fragment = document.createDocumentFragment();
+  console.log(data.centres);
+  for (const center of data.centres) {
+    for (const key in center) {
+      if (key !== "id") {
+        const child = document.createElement("p");
+        child.textContent = center[key];
+        fragment.appendChild(child);
+      }
+    }
+  }
+  detailContainer.append(fragment);
+}
+
+getISROCentersData();
